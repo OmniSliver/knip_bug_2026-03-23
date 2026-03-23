389c7a36c5bce2ad9b644b8de3697b5ec2e5080a
Patricio Merino <patitomv@gmail.com>
2026-03-23 15:57:27 -0300

    Basic app

---
+++ bootstrap/prodServer.mjs
@@ -0,0 +1,21 @@
+import express from 'express'
+import path from 'node:path'
+
+const port = 3000
+const address = '::'
+
+const app = express()
+
+const webRoot = path.join(process.cwd(), 'www')
+
+app.use('/', express.static(webRoot))
+app.use('/admin', express.static(webRoot))
+
+app.listen(port, address, error => {
+  if (error) {
+    console.error('prodServer app listen error:', error)
+    process.exit(1)
+  }
+
+  console.info(`Server running on ${address}:${port}`)
+})
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import path from 'node:path'
+
+export default {
+  entry: './bootstrap/browser.jsx',
+
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        exclude: [path.resolve(import.meta.dirname, 'node_modules/')],
+        test: /\.(ts|js)x?$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: '.cache/babelLoader',
+              presets: [
+                '@babel/preset-env',
+                ['@babel/react', { runtime: 'automatic' }],
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve('www'),
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      scriptLoading: 'module',
+      template: 'bootstrap/index.html',
+    }),
+  ],
+}
